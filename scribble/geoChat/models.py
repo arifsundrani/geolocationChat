4ccbc0bab9de4c7e8d223eabe99c03c62c68b5f0
meera1hahn <meera1hahn@gmail.com>
2015-03-05 17:46:53 -0500

    Initialized Project
    made the app for Chat Page and created some of the basic models

---
+++ scribble/geoChat/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Page(models.Model):
+    createDate = models.DateTimeField('date created')
+    topic = models.CharField(max_length = 60)
+    createUser = models.User('created this user')
+    location = models.ForeignKey(RegionCoordinates)
+
+class Comment(models.Model):
+    text = models.CharField(max_length= 200)
+    postDate = models.DateTimeField('date published')
+    poster = models.User('person posting')
+
+class RegionCoordinates(models.Model):
+    x1 = models.FloatField()
+    y1 = models.FloatField()
+    x2 = models.FloatField()
+    y2 = models.FloatField()
+
+
+
