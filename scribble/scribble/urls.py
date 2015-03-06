ebb41b311f7faf2e2ed4adcf825c60ebc1572c7a
Shureed <skabir1717@gmail.com>
2015-03-05 21:04:10 -0500

    test

--- scribble/scribble/urls.py
+++ scribble/scribble/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'scribble.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # test
 
     url(r'^admin/', include(admin.site.urls)),
 )
