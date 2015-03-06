a527c2d5f9b8932970d2974a284262daa8a12481
Arif Sundrani <arifsundrani@gmail.com>
2015-03-05 20:42:32 -0500

    Updated views.py
    Added specific code to create classes based on the current models.

--- scribble/geoChat/views.py
+++ scribble/geoChat/views.py
@@ -1,3 +1,43 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.shortcuts import render
+from geoChat.models import Page, Comment, RegionCoordinates
+from django.core.urlresolvers import reverse
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DeleteView,
+    UpdateView,
+    DetailView)
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+class  ChatView(object):
+	"""docstring for  ChatView"""
+	def __init__(self, arg):
+		super( ChatView, self).__init__()
+		self.arg = arg
+
+class ProfileView(object):
+	"""docstring for ProfileView"""
+	def __init__(self, arg):
+		super(ProfileView, self).__init__()
+		self.arg = arg
+		
+class ChatListView(object):
+	"""docstring for ChatListView"""
+	def __init__(self, arg):
+		super(ChatListView, self).__init__()
+		self.arg = arg
+
+class SettingsView(object):
+			"""docstring for SettingsView"""
+			def __init__(self, arg):
+				super(SettingsView, self).__init__()
+				self.arg = arg
+
+class CreateChatView(object):
+	"""docstring for CreateChatView"""
+	def __init__(self, arg):
+		super(CreateChatView, self).__init__()
+		self.arg = arg
