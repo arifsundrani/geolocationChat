e7431a86e8968d4251be88a232f3144862c20bef
Shureed <skabir1717@gmail.com>
2015-03-17 17:22:17 -0400

    added urls and gitignore

--- scribble/scribble/urls.py
+++ scribble/scribble/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+#REST
 from django.conf.urls import url, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
+#twisted
+from django.views.generic import RedirectView
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -23,6 +26,8 @@ router.register(r'users', UserViewSet)
 
 
 urlpatterns = patterns('',
+    url(r'^(/)?$', RedirectView.as_view(url='/chat/')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^chat/', include('geoChat.urls')),
 )
