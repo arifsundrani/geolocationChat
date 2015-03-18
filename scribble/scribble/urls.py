2737803f9b2043ce4313d2135402671fd9f1dee0
meera1hahn <meera1hahn@gmail.com>
2015-03-18 18:46:35 -0400

    base pages
    html pages

--- scribble/scribble/urls.py
+++ scribble/scribble/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+import geoChat.views
 
 #REST
 from django.conf.urls import url, include
@@ -28,7 +29,7 @@ router.register(r'users', UserViewSet)
 '''
 
 urlpatterns = patterns('',
-    url(r'^(/)?$', RedirectView.as_view(url='//')),
+    url(r'^(/)?$', geoChat.views.HomeView.as_view(), name = 'home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login/$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout'),
