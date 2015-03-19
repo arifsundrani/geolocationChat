03f587ae07159badf67e5cebeab881a381ef9c1e
Arif Sundrani <arifsundrani@gmail.com>
2015-03-19 14:11:06 -0400

    Updated tests.py
    updated tests.py to attempt to test certain scenarios.

--- scribble/geoChat/tests.py
+++ scribble/geoChat/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.test.utils import setup_test_environment
+from django.db import models
+from django.contrib.auth.forms import User
+from django.utils import timezone
+from geoChat.models import Comment
 
-# Create your tests here.
+class CommentTest(TestCase):
+	#def setUp(self):
+		
+	def test_comment_was_made(self):
+		c = Comment.objects.create(text = 'This chat room is wicked cool.', postDate = timezone.now(), name='AnonymousUser')
+		c.save()
+		self.assertEqual(c.name, 'AnonymousUser')
+		self.assertEqual(c.text, 'This chat room is wicked cool.')
+	
+#class LoginTest(TestCase):
+#	c = Client()
+#	response = c.post('/login/',{'username':'arif','password':'nope'})
+#	response.status_code
+	
+#class SignupViewTest(TestCase):
+
+#class ChatRoomViewTest(TestCase):
+	
