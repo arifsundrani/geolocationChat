ea0713eaac57323210222cb7e2e78062e0e7fde3
Shureed <skabir1717@gmail.com>
2015-03-25 18:41:12 -0400

    have user move to appropriate chat room

--- scribble/geoChat/models.py
+++ scribble/geoChat/models.py
@@ -21,19 +21,23 @@ class Comment(models.Model):
     text = models.CharField(max_length= 200)
     postDate = models.DateTimeField('date published')
     name = models.CharField(max_length=12)
+
     def __unicode__ (self):
         return self.text
     #poster = models.ForeignKey(User, blank=True, null=True)
 
 #chat room
 
+
 class ChatRoom(models.Model):
     name = models.CharField(max_length=50)
-    messages = []
+    clients = []
 
     def __unicode__(self):
         return self.name
 
+    def __init__(self, *args, **kwargs):
+        self.messages = []
 
 
 
