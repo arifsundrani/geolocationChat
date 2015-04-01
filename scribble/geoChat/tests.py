09846e66308776fbfe33165d4e451cbad7dd8133
Arif Sundrani <arifsundrani@gmail.com>
2015-04-01 13:50:22 -0400

    Updated tests.py
    Updated and added a test to the tests.py file. This should work for the
    web hook

--- scribble/geoChat/tests.py
+++ scribble/geoChat/tests.py
@@ -3,26 +3,33 @@ from django.test.utils import setup_test_environment
 from django.db import models
 from django.contrib.auth.forms import User
 from django.utils import timezone
-from geoChat.models import Comment
+from geoChat.models import Comment, ChatRoom
 
-class CommentTest(TestCase):		
-	def test_comment_was_made(self):
-		c = Comment.objects.create(text = 'This chat room is wicked cool.', postDate = timezone.now(), name='AnonymousUser')
-		c.save()
-		self.assertEqual(c.name, 'AnonymousUser')
-		self.assertEqual(c.text, 'This chat room is wicked cool.')
-	
+
+
+class ChatRoomTest(TestCase):
+    def test_room_was_made(self):
+        a = ChatRoom.objects.create(name = 'Emory')
+        a.save()
+        self.assertEqual(a.name, 'Emory')
+        self.assertEqual(a.active, True)
+
+class CommentTest(TestCase):C
+    def test_comment_was_made(self):
+        c = Comment.objects.create(text='This chat room is wicked cool.', postDate=timezone.now(), user_name='AnonymousUser')
+        c.save()
+        self.assertEqual(c.user_name, 'AnonymousUser')
+        self.assertEqual(c.active, True)
+        self.assertEqual(c.text, 'This chat room is wicked cool.')
+
 class LoginTest(TestCase):
-	def test_login(self):
-		u = Client()
-		response = u.post('/login/',{'username':'arif','password':'nope'})
-		response.status_code
-	
-class SignupViewTest(TestCase):
-	def test_register(self):
-		u = Client()
-		response = u.post('/register/',{'username':'arif','password1':'nope', 'password2': 'nope'})
-		response.status_code
+    def test_login(self):
+        u = Client()
+        response = u.post('/login/',{'username':'test','password':'nope'})
+        response.status_code
 
-#class ChatRoomViewTest(TestCase):
-	
+class RegisterTest(TestCase):
+    def test_register(self):
+        v = Client()
+        response = v.post('/register/',{'username':'arif','password1':'nope', 'password2': 'nope'})
+        response.status_code
