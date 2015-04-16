ed19b208b91bb14247a882975b718d37feb2f2e7
Arif Sundrani <arifsundrani@gmail.com>
2015-04-16 16:20:53 -0400

    Revert ec83dc9..08d8237
    This rolls back to commit ec83dc92ae6932c4387631f95a5ea850cd442199.

--- scribble/geoChat/views.py
+++ scribble/geoChat/views.py
@@ -5,7 +5,6 @@ from geoChat.models import Page, Comment, RegionCoordinates
 from django.core.urlresolvers import reverse
 from django.views.generic import View
 from django.views.generic import FormView
-from django import forms
 import sys
 from django.template import Context, Template
 from django.db.models import F
@@ -44,9 +43,6 @@ def chat_room(request, chat_room_id):
         'first' : first,
     }
     return render(request, 'chats/chat_room.html', context)
-
-def about(request):
-    return render(request, 'registration/about.html')
 
 def chat_room2(request):
     #chat_rooms = ChatRoom.objects.filter(lat1__lte= float(request.POST.get('lat',False))).filter(long1__lte= request.POST.get('long',False)).filter(lat2__gte= request.POST.get('lat',False)).filter(long2__gte= request.POST.get('long',False)).order_by('name')
@@ -114,7 +110,12 @@ class CreateRegisterView(FormView):
         username = self.request.POST['username']
         password = self.request.POST['password1']
         user = authenticate(username=username, password=password)
-        login(self.request, user)
+        if validateEmail(self.request.POST['email']):
+            login(self.request, user)
+            return super(CreateRegisterView, self).form_valid(form)
+        else:
+            raise ValidationError("Email address is not real")
+
         return super(CreateRegisterView, self)
 
 def create_chat_room(request):
@@ -128,5 +129,3 @@ def validateEmail(email):
         return True
     except ValidationError:
         return False
-def password_reset(request):
-    return password_reset(request)
